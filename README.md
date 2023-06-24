efed9d8ee6c01e04a954f19681212ea9b84d8cb1
cymaob <128226579+cymaob@users.noreply.github.com>
2023-06-24 12:11:48 +0200

    finished work and added readme

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # TravelBot
+
+TravelBot is a chatbot that provides basic assistance in finding a destination for your next vacation. It interacts with the user through a series of questions and suggestions.
+
+## Features
+
+- Asks for the user's name and greets them accordingly.
+- Helps the user find a destination based on their preferences.
+- Supports neighboring countries of Switzerland: France, Germany, Austria, Liechtenstein, and Italy.
+- Provides suggestions for cities within each country.
+- Handles positive, negative, and exit responses from the user.
+
+## Usage
+
+1. Clone the repository: https://github.com/cymaob/TravelBot
+2. Navigate to the project directory
+3. Run the Python script
+
+
+## Answer Limitations
+There are some limitations regarding the possibilities of answers. Please find the keywords below:
+
+- Positive responses: yes, great, of course, good and go ahead
+- Negative responses: no, nope, nah, not yet, not a chance and don't know
+- Exit commands: quit, pause, exit, goodbye, bye, later, stop and bubye
+
+Please keep in mind these limitations while interacting with TravelBot.
+
+## Development
+
+More countries, more cities as well as more detailed explanation about cities can be added.
+
+## About
+
+This project was developed by [Cyril Oberlin](https://github.com/cymaob).
+
+
--- TravelBot.py
+++ TravelBot.py
@@ -2,39 +2,66 @@ import re
 import random
 
 class Chatbot:
-        #negative responses
-        negativeResponses = ("no", "nope", "nah", "naw", "not a chance", "sorry")
-        #exit commands
+        name = "You: "
+        botName = "TravelBot: "
+        negativeResponse = ("no", "nope", "nah", "not yet", "not a chance", "don't know")
+        positiveResponse = ("yes", "great", "of course", "good", "go ahead")
         exitCommands = ("quit", "pause", "exit", "goodbye", "bye", "later", 'stop', 'bubye')
-        #random starter questions
-        starterQuestions = (
-                "Where would you like to travel to? \n",
-                "What country would you like to go? \n"
-                #"Is there anything I can do for you? \n",
-                #"What would yo like me to help you with? \n"
+        nameQuestions = (
+                "Whats your name? \n",
+                "What should I call you? \n",
+                "I'm curious, what's your name? \n",
+                "Would you be so kind as to reveal your name to me? \n"
         )
+
+        niceToMeet = (
+                "Nice to meet you ",
+                "It's a pleasure to meet you ",
+                "Happy to help you "
+        )
+
+        starterQuestions = (
+                "Do you already have a country in mind? \n",
+                "Do you already have an idea of where you want to go? \n",
+                "Have you decided on a particular country you'd like to go? \n"
+        )
+
         byeMessages = (
                 "Ok, have a nice day! \n",
                 "Happy to help you another time! Enjoy your day! \n",
-                "See you soon! \n"
+                "Ok, see you soon! \n"
         )
 
+        countries = ("France","Germany","Austria","Liechtenstein","Italy")
+
+        France = ("Paris", "Marseille", "Lyon", "Strasbourg")
+        Germany = ("Berlin", "Hamburg", "Stuttgart", "Bremen", "Munich")
+        Austria = ("Graz", "Salzburg", "Wien", "Innsbruck", "Linz")
+        Liechtenstein = ("Vaduz")
+        Italy = ("Milan", "Florence", "Rome", "Venice", "Naples")      
 
         def __init__(self):
                 self.dialog = {
-                        'france': r'.*\s*france.*',
-                        'germany': r'.*\s*germany.*',
-                        'austria': r'.*\s*austria.*',
-                        'liechtenstein': r'.*\s*liechentenstein.*',
-                        'italy': r'.*\s*italy.*',
+                        'france': r'^\s*france.*',
+                        'germany': r'^\s*germany.*',
+                        'austria': r'^\s*austria.*',
+                        'liechtenstein': r'^\s*liechentenstein.*',
+                        'italy': r'^\s*italy.*',
                 }
                 
         
         def chat(self):
-                reply = input("TravelBot: " + random.choice(self.starterQuestions)).lower()
-                if not self.exit(reply):
-                        reply = input(self.matchReply(reply))             
+                print(self.botName + "Welcome to Travelbot! Let me help you find a destination for your journey.")
+                self.name = input(self.botName + random.choice(self.nameQuestions) + self.name)
+                print(self.botName + random.choice(self.niceToMeet) + self.name + "!")
+                self.start()
+                
 
+        def start(self):
+                reply = input(self.botName + random.choice(self.starterQuestions) + self.name + ": ")
+                if not self.exit(reply) and not self.noIdea(reply):
+                        reply = input(self.matchReply(reply))
+                        
 
         def exit(self, reply):
                 for exitMessage in self.exitCommands:
@@ -43,7 +70,22 @@ class Chatbot:
                                 return True
                         return False
                                 
+        
+        def noIdea(self, reply):
+                country = random.choice(self.countries)
+                responses =(
+                        "No worries, I'll help you. It would be a great idea to go to " + country + ".",
+                        "Ok, Let me give you an idea: " + country + " is very beautiful and would be a great choice.",
+                        country + " would be a great place to visit!",
+                        country + " would be a good choice!"
+                )
+                for negativeMessage in self.negativeResponse:
+                        if negativeMessage in reply.lower():
+                                print(self.botName + random.choice(responses))
+                                return self.cityReply(country)
+                return None
 
+                
         def matchReply(self, reply):
                 for key, value in self.dialog.items():
                         intent = key
@@ -53,46 +95,98 @@ class Chatbot:
                         if foundMatch and intent == 'france':
                                 country = "France"
                                 print(self.countryReply(country))
-                                print(self.cityReply(country))
 
                         elif foundMatch and intent == 'germany':
                                 country = "Germany"
-                                return self.countryReply(country)
+                                print(self.countryReply(country))
                         
                         elif foundMatch and intent == 'austria':
                                 country = "Austria"
-                                return self.countryReply(country)
+                                return (self.countryReply(country))
                         
                         elif foundMatch and intent == 'liechtenstein':
                                 country = "Liechtenstein"
-                                return self.countryReply(country)
+                                return (self.countryReply(country))
                         
                         elif foundMatch and intent == 'italy':
                                 country = "Italy"
-                                return self.countryReply(country)
+                                return (self.countryReply(country))
                         
 
         def countryReply(self, country):
                 responses = (
                         "It's a great idea to go to " + country + "! ",
+                        country + " is a great choice!"
                 )
-                return ("TravelBot: " + random.choice(responses))
+                print ("TravelBot: " + random.choice(responses))
+                return self.cityReply(country)
         
-        def cityReply(self, country):
-                France = ("Paris", "Marseille", "Lyon", "Nice")
-                Germany = ("Berlin", "Hamburg", "Stuttgart", "Bremen", "Munich")
-                Austria = ("Graz", "Salzburg", "Wien", "Innsbruck", "Linz")
-                Liechtenstein = ("Vaduz")
-                Italy = ("Milan", "Florence", "Rome", "Venice", "Naples")        
+
+        def cityReply(self, country):  
+                if country == "France":
+                        city = random.choice(self.France)
+                elif country == "Austria":
+                        city = random.choice(self.Austria)
+                elif country == "Italy":
+                        city = random.choice(self.Italy)
+                elif country == "Liechtenstein":
+                        city = random.choice(self.Liechtenstein)
+                elif country == "Germany":
+                        city = random.choice(self.Germany)
 
                 responses = (
-                        "You could go to " + random.choice(France) + ". It is very nice there!",
+                        "You could go to " + city + ". It is very nice there!",
+                        city + " would be an amazing choice!",
+                        "I can recommend you to go to " + city + "!"
                 )
-
-                return "TravelBot: " + random.choice(responses)
+                reply = input(self.botName + "Would you like me to propose you a city in " + country +"? \n" + self.name + ": ")
+                if not self.exit(reply):
+                        if self.negResponse(reply):
+                                return self.again()
+                        elif self.posResponse(reply):
+                                print(self.botName + random.choice(responses))
+                                return self.again()
+                        else: return self.noMatch()
                 
+        def again(self):
+                responses = (
+                        "Would you like to start again? \n",
+                        "Can I assist you with another idea? \n"
+                )
+                againMessage = (
+                        "Ok, let's find another idea specifically for you!",
+                        "Sure, I'm happy to help you again!"
+                )
+                reply = input(self.botName + random.choice(responses) + self.name + ": ")
+                if not self.exit(reply):
+                        if self.negResponse(reply):
+                                print(self.botName + random.choice(self.byeMessages))
+                                exit()
+                        elif self.posResponse(reply):
+                                print(self.botName + random.choice(againMessage))
+                                return self.start()
+                        else: self.noMatch()
 
 
+        def negResponse(self, reply):
+                for negativeReply in self.negativeResponse:
+                        if negativeReply in reply.lower():
+                                return True
+                        return False
+
+        def posResponse(self, reply):
+                for positiveReply in self.positiveResponse:
+                        if positiveReply in reply.lower():
+                                return True
+                        return False
+
+
+        def noMatch(self):
+                responses =(
+                        "I'm sorry, I didn't understand that. Please try again \n",
+                        "Unfortunately I cannot understand your answer. Please try again. \n"
+                )
+                return (self.botName + random.choice(responses))
 
 
 # create intance and start chat
